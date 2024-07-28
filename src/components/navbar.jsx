ae89f1e30fb629805a2b34fa43528a5c596db158
Vijesh Shetty <vijeshsshetty@gmail.com>
2024-07-28 22:03:09 +0530

    added navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import HamburgerBtn from "./hamburgerBtn";
+const Navbar = () => {
+  return (
+    <nav className="flex justify-between lg:justify-start px-6 lg:pr-12 pt-3 pb-3 bg-white">
+      <div className="lg:w-1/4 font-semibold">Augmented Labs</div>
+      <HamburgerBtn />
+      <ul className="hidden lg:flex justify-between w-3/4 font-medium">
+        <li className="cursor-pointer underline-animation">Home</li>
+        <li className="cursor-pointer underline-animation">About</li>
+        <li className="cursor-pointer underline-animation">Services</li>
+        <li className="cursor-pointer underline-animation">Contact Us</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
