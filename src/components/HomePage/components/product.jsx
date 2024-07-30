53cf7cfb7a2062998c504975eb7843669b0455f7
Vijesh Shetty <vijeshsshetty@gmail.com>
2024-07-30 12:56:57 +0530

    three js added

--- src/components/HomePage/components/product.jsx
+++ src/components/HomePage/components/product.jsx
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import * as THREE from 'three';
 
 const LatestProduct = () => {
-  return <div className="p-4 h-full"></div>;
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const container = containerRef.current;
+    let width = container.clientWidth - 60;
+    let height = container.clientHeight - 40;
+
+    // init
+    const camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 10);
+    camera.position.z = 1;
+
+    const scene = new THREE.Scene();
+
+    const geometry = new THREE.BoxGeometry(0.45, 0.45, 0.45);
+    const material = new THREE.MeshNormalMaterial();
+
+    const mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(width, height);
+    container.appendChild(renderer.domElement);
+
+    // handle window resize
+    const handleResize = () => {
+      width = container.clientWidth - 60;
+      height = container.clientHeight - 40;
+      camera.aspect = width / height;
+      camera.updateProjectionMatrix();
+      renderer.setSize(width, height);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // animation
+    function animate(time) {
+      mesh.rotation.x = time / 2000;
+      mesh.rotation.y = time / 1000;
+
+      renderer.render(scene, camera);
+    }
+
+    renderer.setAnimationLoop(animate);
+
+    return () => {
+      renderer.setAnimationLoop(null);
+      window.removeEventListener('resize', handleResize);
+      container.removeChild(renderer.domElement);
+    };
+  }, []);
+
+  return (
+    <div className="p-4 h-full flex justify-center items-center" ref={containerRef}></div>
+  );
 };
 
 export default LatestProduct;
